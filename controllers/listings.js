3862abe4153c58a8983f15d36f81c31fcdfb0fff
Smritirekha Kar <justsmritirekha@gmail.com>
2026-04-21 18:12:42 +0530

    Fix: populate owner in listing show route and add null check in EJS

--- controllers/listings.js
+++ controllers/listings.js
@@ -37,9 +37,11 @@ module.exports.renderNewForm = (req, res) => {
 
 module.exports.showListing = async (req, res) => {
   let { id } = req.params;
-  const listing = await Listing.findById(id)
-    .populate({ path: "reviews", populate: { path: "author" } })
-    .populate("owner");
+  const listing = await Listing.findById(req.params.id)
+    .populate("owner")
+    .populate({ path: "reviews", populate: { path: "author" } 
+    });
+   
   if (!listing) {
     req.flash("error", "Listing does not exist!");
     return res.redirect("/listings");
